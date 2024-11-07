11ad03c835f7c912968c549219722b5fe0faf150
Francesco Micheli <francescomicheli@proton.me>
2024-11-07 11:25:37 +0400

    Add Fitness goals

--- personaltrainer-rs/src/main.rs
+++ personaltrainer-rs/src/main.rs
@@ -5,8 +5,11 @@ use std::env;
 use dotenv::dotenv;
 
 mod models;
+mod utils;
 
-use models::{User, Gender, FitnessLevel};
+
+use utils::get_user_input;
+use models::{User, Gender, FitnessLevel, FitnessGoal};
 
 
 #[derive(Serialize)]
@@ -21,6 +24,7 @@ struct GenerateResponse {
     response: Option<String>,
 }
 
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     
@@ -30,34 +34,82 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Create an HTTP client
     let client = reqwest::Client::new();
 
-    // Get API endpoint and model name from environment variable or use default
+    // Get API endpoint and model name from environment variable
     let api_endpoint = env::var("OLLAMA_API_ENDPOINT").expect("OLLAMA_API_ENDPOINT must be set");
     let model_name = env::var("OLLAMA_MODEL").expect("OLLAMA_MODEL must be set");
 
+    // Build user object
+    let name = get_user_input("\nEnter your first name: ");
+    let surname = get_user_input("Enter your last name: ");
+    let age = get_user_input("Enter your age: ").parse::<u8>().unwrap_or(0);
+    let height = get_user_input("Enter your height in cm: ").parse::<f32>().unwrap_or(0.0);
+    let weight = get_user_input("Enter your weight in kg: ").parse::<f32>().unwrap_or(0.0);
+    
+    println!("\nSelect your gender:");
+    println!("1. Male");
+    println!("2. Female");
+    let gender = match get_user_input("Enter number (1-2): ").as_str() {
+        "1" => Gender::Male,
+        "2" => Gender::Female,
+        _ => Gender::Male,
+    };
 
-    // Create a user
+    println!("\nSelect your fitness level:");
+    println!("1. Beginner");
+    println!("2. Intermediate");
+    println!("3. Advanced");
+    let fitness_level = match get_user_input("Enter number (1-3): ").as_str() {
+        "1" => FitnessLevel::Beginner,
+        "2" => FitnessLevel::Intermediate,
+        "3" => FitnessLevel::Advanced,
+        _ => FitnessLevel::Beginner,
+    };
+
+    let physical_limitations = get_user_input("Enter any physical limitations (or press Enter if none): ");
+    let time_available = get_user_input("How many hours per week can you dedicate to exercise? ").parse::<f32>().unwrap_or(3.0);
+    let equipment = get_user_input("What equipment do you have access to? (press Enter if none) ");
+    // let fitness_goal = get_user_input("What is your fitness goal? (press Enter if none)");
+    let fitness_goal = match get_user_input("\n
+    1. Weight loss
+    2. Muscle gain
+    3. Endurance
+    4. Flexibility
+    5. Balance
+    6. Other
+    Enter number (1-6): ").as_str() {
+        "1" => FitnessGoal::WeightLoss,
+        "2" => FitnessGoal::MuscleGain,
+        "3" => FitnessGoal::Endurance,
+        "4" => FitnessGoal::Flexibility,
+        "5" => FitnessGoal::Balance,
+        "6" => FitnessGoal::Other,
+        _ => FitnessGoal::Other,
+    };
+    
+
     let user = User::new(
-        "John".to_string(),
-        "Doe".to_string(),
-        30,
-        180.0,
-        70.0,
-        Gender::Male,
-        FitnessLevel::Beginner,
-        "".to_string(),
-        3.0,
-        "".to_string(),
+        name,
+        surname,
+        age,
+        height,
+        weight,
+        gender,
+        fitness_level,
+        physical_limitations,
+        time_available,
+        equipment,
+        fitness_goal,
     );
 
-
     let user_prompt = format!("I want you to act as a personal trainer. 
-    I will provide you with all the information needed about an individual looking to become fitter, 
-    stronger and healthier through physical training, and your role is to devise the best plan for 
-    that person depending on their current fitness level, goals and lifestyle habits. 
+    I will provide you with all the information needed about me, because I am looking to become fitter, 
+    stronger and healthier through physical training, and your role is to devise the best plan for me 
+    depending on my current fitness level, goals and lifestyle habits. 
     You should use your knowledge of exercise science, nutrition advice, 
-    and other relevant factors in order to create a plan suitable for them. The person name is {} {}, he's {} years old, {}cm tall, he weights {}kg. 
-    His fitness level is {}. 
-    He has {} physical limitations, {} time available, and {} equipment.", user.name, user.surname, user.age, user.height, user.weight, user.fitness_level.to_string(), user.physical_limitations, user.time_available, user.equipment);
+    and other relevant factors in order to create a plan suitable for me. 
+    My name is {} {}, I'm {} years old, {}cm tall, I weights {}kg. 
+    My fitness level is {}. I have {} as a physical limitations, {} hours a week available, and {} equipment.", 
+    user.name, user.surname, user.age, user.height, user.weight, user.fitness_level.to_string(), user.physical_limitations, user.time_available, user.equipment);
 
 
     // Prepare the request payload
@@ -66,6 +118,8 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         prompt: user_prompt,
         stream: false,
     };
+
+    println!("\nWaiting for the Personal TrAIner to generate a plan...\n");
 
     // Send POST request to Ollama API
     let response = client
@@ -78,7 +132,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let result: GenerateResponse = response.json().await?;
     
     if let Some(response_text) = &result.response {
-        println!("Response: {}", response_text);
+        println!("\nResponse: {}", response_text);
     } else {
         eprintln!("No response field in the API result");
     }
--- personaltrainer-rs/src/models.rs
+++ personaltrainer-rs/src/models.rs
@@ -1,3 +1,6 @@
+use crate::utils::get_user_input;
+
+#[allow(dead_code)]
 #[derive(Debug)]
 pub struct User {
     pub name: String,
@@ -10,12 +13,14 @@ pub struct User {
     pub physical_limitations: String,
     pub time_available: f32,
     pub equipment: String,
+    pub fitness_goal: FitnessGoal,
 }
 
 
+#[allow(dead_code)]
 impl User {
-    pub fn new(name: String, surname: String, age: u8, height: f32, weight: f32, gender: Gender, fitness_level: FitnessLevel, physical_limitations: String, time_available: f32, equipment: String) -> Self {
-        User { name, surname, age, height, weight, gender, fitness_level, physical_limitations, time_available, equipment }
+    pub fn new(name: String, surname: String, age: u8, height: f32, weight: f32, gender: Gender, fitness_level: FitnessLevel, physical_limitations: String, time_available: f32, equipment: String, fitness_goal: FitnessGoal) -> Self {
+        User { name, surname, age, height, weight, gender, fitness_level, physical_limitations, time_available, equipment, fitness_goal }
     }
 
     pub fn to_string(&self) -> String {
@@ -31,6 +36,7 @@ pub enum Gender {
 }
 
 
+#[allow(dead_code)]
 impl Gender {
     pub fn to_string(&self) -> String {
         match self {
@@ -55,6 +61,33 @@ impl FitnessLevel {
             FitnessLevel::Beginner => "beginner".to_string(),
             FitnessLevel::Intermediate => "intermediate".to_string(),
             FitnessLevel::Advanced => "advanced".to_string(),
+        }
+    }
+}
+
+#[derive(Debug)]
+pub enum FitnessGoal {
+    WeightLoss,
+    MuscleGain,
+    Endurance,
+    Flexibility,
+    Balance,
+    Other,
+}
+
+#[allow(dead_code)]
+impl FitnessGoal {
+    pub fn to_string(&self) -> String {
+        match self {
+            FitnessGoal::WeightLoss => "weight loss".to_string(),
+            FitnessGoal::MuscleGain => "muscle gain".to_string(),
+            FitnessGoal::Endurance => "endurance".to_string(),
+            FitnessGoal::Flexibility => "flexibility".to_string(),
+            FitnessGoal::Balance => "balance".to_string(),
+            FitnessGoal::Other => {
+                let user_input = get_user_input("Please specify your fitness goal not included in the list: ");
+                user_input
+            },
         }
     }
 }
